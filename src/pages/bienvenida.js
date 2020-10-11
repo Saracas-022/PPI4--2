7b3679c378b1958046172c82293b6037083a480c
Sara Castañeda Londoño <66443629+Saracas-022@users.noreply.github.com>
2020-10-10 19:32:40 -0500

    Pantallas iniciales

---
+++ src/pages/bienvenida.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/style.css";
+import Apertura from "../pages/apertura";
+
+class bienvenida extends React.Component {
+  render() {
+    return (
+      <div>
+        <Apertura />
+      </div>
+    );
+  }
+}
+export default bienvenida;
---
+++ src/routers/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Apertura from "../pages/apertura";
+import Registro from "../pages/registro";
+import Menu from "../pages/menu";
+import Bienvenida from "../pages/bienvenida";
+import Inicio from "../pages/inicio";
+import Inicio_admin from "../pages/inicio_admin";
+import Perfil from "../pages/perfil";
+import IniciarS from "../pages/iniciarS";
+import ListaDulce from "../pages/listaDulce";
+import ListaSalado from "../pages/listaSalado";
+import ListaDulce_admin from "../pages/listaDulce_admin";
+import ListaSalado_admin from "../pages/listaSalado_admin";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Apertura} />
+        <Route exact path="/registro" component={Registro} />
+        <Route exact path="/menu" component={Menu} />
+        <Route exact path="/menu" component={Bienvenida} />
+        <Route exact path="/inicio" component={Inicio} />
+        <Route exact path="/perfil" component={Perfil} />
+        <Route exact path="/inicio_admin" component={Inicio_admin} />
+        <Route exact path="/iniciarS" component={IniciarS} />
+        <Route exact path="/listaDulce" component={ListaDulce} />
+        <Route exact path="/listaSalado" component={ListaSalado} />
+        <Route exact path="/listaDulce_admin" component={ListaDulce_admin} />
+        <Route exact path="/listaSalado_admin" component={ListaSalado_admin} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
